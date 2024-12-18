f9f96c80ad507ed39883d261a016926e397558f2
dylansun7 <dylansun7@163.com>
2024-12-18 15:47:07 +0800

    web

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+<!-- eslint-disable vue/no-parsing-error -->
 <template>
  <div class="fa">
     <div class="outside">
     <div class="son">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
-        <table align="center"  width="600px" cellpadding="8" >
+        <table align="center"  width="600px" cellpadding="8" border="1">
             <tbody>
             <tr align="center" width="400px">
                 <th colspan="5">
@@ -19,21 +20,21 @@
             </tr>
             <tr >
                 <th >
-                    <div ref="term" @click.stop="isshow = !isshow">
+                    <div ref="term1" @click.stop="isshow1 = !isshow1">
                         <div><button class="dropbtn">数据处理工具</button></div>
                     </div>
-                    <div v-show="isshow" class="list" ref="drop">
+                    <div v-show="isshow1"  ref="drop1">
 
                             <!-- 功能1.1：图片损坏检测 -->
-                        <ul style="list-style-type: none;">
+                        
                             <li>
                                 <div>
                                     <div class="mask" v-if="box11" @click="box11 = false"></div>      <!--修改对应的模组尾数box_,必须相同-->
                                     <div class="pop" v-if="box11">
-                                        <button @click="box11 = false" class="close-btn"><span class="close-span">&times;</span></button>
+                                        <button @click="box11 = false" class="close-btn">&times;</button>
                                         <check_file :someProp="checkfilevalue" @custom-event="handleCheckfileEvent" />  <!--功能链接-->
                                     </div>
-                                    <button @click="box11 = true" class="btn">图片损坏检测</button> <!--功能标签按钮-->
+                                    <button @click="box11 = true" class="btn">>>图片损坏检测<<</button> <!--功能标签按钮-->
                                 </div>
                             </li>
 
@@ -45,7 +46,7 @@
                                         <button @click="box12 = false" class="close-btn">&times;</button>
                                         <split_data :someProp="splitdatavalue" :split-ratio="splitRatio" @custom-event="handleSplitdataEvent" />  <!--功能链接-->
                                     </div>
-                                    <button @click="box12 = true" class="btn">数据切分工具</button> <!--功能标签按钮-->
+                                    <button @click="box12 = true" class="btn">>>数据切分工具<<</button> <!--功能标签按钮-->
                                 </div>
                             </li>
 
@@ -57,7 +58,7 @@
                                         <button @click="box13 = false" class="close-btn">&times;</button>
                                         <cut_video :someProp="cutvideovalue" @custom-event="handleCutvideoEvent" />  <!--功能链接-->
                                     </div>
-                                    <button @click="box13 = true" class="btn">视频抽帧工具</button> <!--功能标签按钮-->
+                                    <button @click="box13 = true" class="btn">>>视频抽帧工具<<</button> <!--功能标签按钮-->
                                 </div>
                             </li>
 
@@ -69,11 +70,11 @@
                                         <button @click="box14 = false" class="close-btn">&times;</button>
                                         <dedup_data :someProp="dedupdatavalue" @custom-event="handleCheckfileEvent" />  <!--功能链接-->
                                     </div>
-                                    <button @click="box14 = true" class="btn">图片去重</button> <!--功能标签按钮-->
+                                    <button @click="box14 = true" class="btn">>>图片去重<<</button> <!--功能标签按钮-->
                                 </div>
                             </li>
 
-                        </ul>
+                        
                     </div>
                 </th>
 
@@ -81,9 +82,10 @@
                     <!--空-->
                 </th>
                 <th>
-                    <div class="dropdown">
+                    <div ref="term2" @click.stop="isshow2 = !isshow2">
                         <button class="dropbtn">算法处理工具</button>
-                        <div class="dropdown-content">
+                    </div>
+                        <div v-show="isshow2"  ref="drop2">
                             
                             <!-- 功能2.1：xxx -->
                             <li style="list-style-type: none;">
@@ -122,15 +124,16 @@
                             </li>
                             
                         </div>
-                    </div>
+                    
                 </th>
                 <th width="2px">
                     <!--空-->
                 </th>
                 <th>
-                    <div class="dropdown">
+                    <div ref="term3" @click.stop="isshow3 = !isshow3">
                         <button class="dropbtn">其他处理工具</button>
-                        <div class="dropdown-content">
+                    </div>
+                        <div v-show="isshow3"  ref="drop3">
                            
                              <!-- 功能3.1：xxx -->
                              <li>
@@ -169,7 +172,7 @@
                             </li>
 
                         </div>
-                    </div>
+                    
                 </th>
             </tr>
         </tbody>
@@ -203,6 +206,7 @@ export default {
       fps: 1,
       fileExt: "someValue",
       dedupdatavalue:" someValue",
+      simThresh: 1,
       box11: false,     /*功能模组匹配按钮*/
       box12: false,
       box13: false,
@@ -213,12 +217,15 @@ export default {
       box31: false,
       box32: false,
       box33: false,
-    //
-      isshow: false,
+      isshow1: false, //
+      isshow2: false,
+      isshow3: false,
     };
   },
   mounted() {
-    document.addEventListener("click", this.clickOut);
+    document.addEventListener("click", this.clickOut1);
+    document.addEventListener("click", this.clickOut2);
+    document.addEventListener("click", this.clickOut3);
   },
   methods: {
     handleCheckfileEvent(data) {
@@ -231,17 +238,43 @@ export default {
         console.log('Custom event triggered with:', data);
     },
     //当点击菜单以外的区域隐藏菜单
-    clickOut(e) {
-      let drop = this.$refs.drop;
-      let term = this.$refs.term;
+    clickOut1(e) {
+      let drop1 = this.$refs.drop1;
+      let term1 = this.$refs.term1;
       if (
-        drop &&
-        !drop.contains(e.target) &&
-        term &&
-        !term.contains(e.target) &&
-        this.isshow
+        drop1 &&
+        !drop1.contains(e.target) &&
+        term1 &&
+        !term1.contains(e.target) &&
+        this.isshow1
       ) {
-        this.isshow = false;
+        this.isshow1 = false;
+      }
+    },
+    clickOut2(e) {
+      let drop2 = this.$refs.drop2;
+      let term2 = this.$refs.term2;
+      if (
+        drop2 &&
+        !drop2.contains(e.target) &&
+        term2 &&
+        !term2.contains(e.target) &&
+        this.isshow2
+      ) {
+        this.isshow2 = false;
+      }
+    },
+    clickOut3(e) {
+      let drop3 = this.$refs.drop3;
+      let term3 = this.$refs.term3;
+      if (
+        drop3 &&
+        !drop3.contains(e.target) &&
+        term3 &&
+        !term3.contains(e.target) &&
+        this.isshow3
+      ) {
+        this.isshow3 = false;
       }
     },
   }
